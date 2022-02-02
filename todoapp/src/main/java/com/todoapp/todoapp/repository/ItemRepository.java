21c8e0b783585a8feaa678eabc833d3c7b953caf
Uğurcan <64015718+uguryt@users.noreply.github.com>
2022-02-02 03:36:53 +0300

    Delete todoapp directory

--- todoapp/src/main/java/com/todoapp/todoapp/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todoapp.todoapp.repository;
- 
-import org.springframework.data.repository.CrudRepository;  
-import com.todoapp.todoapp.entity.Item;  
-public interface ItemRepository extends CrudRepository<Item, Integer>  
-{  
-}  
